9920174cafff94a9d1b808f752afbfda071db46a
Marek Ertinger <me00160@surrey.ac.uk>
2018-07-25 12:33:05 +0100

    HUD completed

--- Main.gd
+++ Main.gd
@@ -21,11 +21,14 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 
 func _on_MobTimer_timeout():
@@ -44,11 +47,11 @@ func _on_MobTimer_timeout():
     # Choose the velocity.
     mob.set_linear_velocity(Vector2(rand_range(mob.min_speed, mob.max_speed), 0).rotated(direction))
 
-
 func _on_ScoreTimer_timeout():
 	score += 1
-
+	$HUD.update_score(score)
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
+
